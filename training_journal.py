6cab0061e233104b71103f79d76b3c3b05bd434b
alexey <eskealexey@mail.ru>
2024-12-01 21:20:11 +0300

    Добавлено:редактирование и удаление записи

--- training_journal.py
+++ training_journal.py
@@ -3,11 +3,12 @@
 """
 import csv
 import tkinter as tk
-from tkinter import ttk, Toplevel, messagebox, Button
+from tkinter import ttk, Toplevel, messagebox, simpledialog
 import re
 import json
 from datetime import datetime
-from tkinter.constants import RIGHT, BOTTOM
+from tkinter.constants import RIGHT
+
 
 # Файл для сохранения данных
 data_file = 'training_log.json'
@@ -26,6 +27,9 @@ def save_data(data):
         json.dump(data, file, indent=4)
 
 class TrainingLogApp:
+    """
+    Класс для ведения дневника тренировок.
+    """
     def __init__(self, root):
         self.root = root
         root.title("Дневник тренировок")
@@ -92,23 +96,23 @@ class TrainingLogApp:
             data = up
         else:
             data = load_data()
-        records_window = Toplevel(self.root)
-        records_window.title("Записи тренировок")
-        place_ = ttk.Frame(records_window, padding=10,relief=tk.RIDGE)
+        self.records_window = Toplevel(self.root)
+        self.records_window.title("Записи тренировок")
+        place_ = ttk.Frame(self.records_window, padding=10,relief=tk.RIDGE)
         place_.pack()
-        tree = ttk.Treeview(place_, columns=("Дата", "Упражнение", "Вес", "Повторения"), show="headings")
-        tree.heading('Дата', text="Дата")
-        tree.heading('Упражнение', text="Упражнение")
-        tree.heading('Вес', text="Вес")
-        tree.heading('Повторения', text="Повторения")
-        scrollbar = ttk.Scrollbar(place_, orient="vertical", command=tree.yview)
-        scrollbar.pack(side=RIGHT, fill=tk.Y)
-        tree.configure(yscrollcommand=scrollbar.set)
-        tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)#
-        btn = ttk.Button(records_window, text="Закрыть", command=records_window.destroy)
+        self.tree = ttk.Treeview(place_, columns=("Дата", "Упражнение", "Вес", "Повторения"), show="headings")
+        self.tree.heading('Дата', text="Дата")
+        self.tree.heading('Упражнение', text="Упражнение")
+        self.tree.heading('Вес', text="Вес")
+        self.tree.heading('Повторения', text="Повторения")
+        self.scrollbar = ttk.Scrollbar(place_, orient="vertical", command=self.tree.yview)
+        self.scrollbar.pack(side=RIGHT, fill=tk.Y)
+        self.tree.configure(yscrollcommand=self.scrollbar.set)
+        self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)#
+        btn = ttk.Button(self.records_window, text="Закрыть", command=self.records_window.destroy)
         btn.pack(side=tk.BOTTOM, pady=10)
 
-        frame1 = ttk.Frame(records_window, borderwidth=1, relief=tk.SOLID, padding=[8, 10])
+        frame1 = ttk.Frame(self.records_window, borderwidth=1, relief=tk.SOLID, padding=[8, 10])
         frame1.pack(side=tk.LEFT, fill=tk.BOTH, anchor=tk.NW)
 
         lbl_select_date = ttk.Label(frame1, text="Введите дату ", font=("Arial", 12), justify=tk.LEFT)
@@ -128,7 +132,7 @@ class TrainingLogApp:
         btn_filter = ttk.Button(frame1, text="Фильтр", command=lambda: self.filter_dates(self.date_entry.get()))
         btn_filter.pack(side=tk.TOP, pady=10)
 
-        frame2 = ttk.Frame(records_window, borderwidth=1, relief=tk.SOLID, padding=[8, 10])
+        frame2 = ttk.Frame(self.records_window, borderwidth=1, relief=tk.SOLID, padding=[8, 10])
         frame2.pack(side=tk.LEFT, fill=tk.BOTH, anchor=tk.NW)
         lbl_select_exercise = ttk.Label(frame2, text="Введите упражнение ", font=("Arial", 12), justify=tk.LEFT)
         lbl_select_exercise.pack(side=tk.TOP, pady=5)
@@ -138,17 +142,25 @@ class TrainingLogApp:
         btn_filter = ttk.Button(frame2, text="Фильтр", command=lambda: self.filter_exercise(self.exercise_entry.get()))
         btn_filter.pack(side=tk.TOP, pady=10)
 
-        frame3 = ttk.Frame(records_window, borderwidth=1, relief=tk.SOLID, padding=[8, 10])
+        frame3 = ttk.Frame(self.records_window, borderwidth=1, relief=tk.SOLID, padding=[8, 10])
         frame3.pack(side=tk.LEFT, fill=tk.BOTH, anchor=tk.NW)
         btn_export = ttk.Button(frame3, text="Экспорт в CSV", command=lambda: self.export_records())
         btn_export.pack(side=tk.TOP, pady=10)
-        btn_import = ttk.Button(frame3, text="Импрт из CSV", command=lambda: self.import_records())
+        btn_import = ttk.Button(frame3, text="Импорт из CSV", command=lambda: self.import_records())
         btn_import.pack(side=tk.TOP, pady=10)
 
-        for entry in data:
-            tree.insert('', tk.END, values=(entry['date'], entry['exercise'], entry['weight'], entry['repetitions']))
+        frame4 = ttk.Frame(self.records_window, borderwidth=1, relief=tk.SOLID, padding=[8, 10])
+        frame4.pack(side=tk.LEFT, fill=tk.BOTH, anchor=tk.NW)
+        btn_edit = ttk.Button(frame4, text="Редактировать запись", command=lambda: self.edit_records(self.tree.selection()))
+        btn_edit.pack(side=tk.TOP, pady=10)
+        btn_delete = ttk.Button(frame4, text="Удалить запись", command=lambda: self.delete_records(self.tree.index(self.tree.selection())))
+        btn_delete.pack(side=tk.TOP, pady=10)
 
-        tree.pack(expand=True, fill=tk.BOTH)
+
+        for entry in data:
+            self.tree.insert('', tk.END, values=(entry['date'], entry['exercise'], entry['weight'], entry['repetitions']))
+
+        self.tree.pack(expand=True, fill=tk.BOTH)
 
     def filter_dates(self, string_filter: str)->None:
         """Фильтр записей по дате."""
@@ -209,10 +221,46 @@ class TrainingLogApp:
                     list_.append(dict(zip(keys, item)))
                 save_data(list_)
                 messagebox.showinfo("Успешно", "Записи успешно импортированы!")
+                self.records_window.destroy()
+                self.view_records(up=list_)
         except FileNotFoundError:
             messagebox.showerror("Ошибка", "Файл не найден!")
 
+    def edit_records(self, selected_item)->None:
+        """Редактирование записей."""
+        item_values = self.tree.item(selected_item, "values")
 
+        new_date = simpledialog.askstring("Редактирование даты", "Введите дату:", initialvalue=item_values[0])
+        new_exersice = simpledialog.askstring("Редактирование упражнения", "Введите новое упражнение:",
+                                         initialvalue=item_values[1])
+        new_weight = simpledialog.askfloat("Редактирование веса", "Введите новый вес:", initialvalue=item_values[2])
+        new_repetitions = simpledialog.askinteger("Редактирование повторений", "Введите новое количество повторений:",
+                                       initialvalue=item_values[3])
+
+        if new_date and new_exersice and new_weight and new_repetitions:
+            # Обновляем запись
+            self.tree.item(selected_item, values=(new_date, new_exersice, new_weight, new_repetitions))
+        data = []
+        for item in self.tree.get_children():
+            item_data = self.tree.item(item)
+            data.append(item_data['values'])  # Сохраняем значения
+        list_ = []
+        keys = ["date", "exercise", "weight", "repetitions"]
+        len_ = len(data)
+        for item in data:
+            print(item)
+            list_.append(dict(zip(keys, item)))
+        save_data(list_)
+        self.records_window.destroy()
+        self.view_records(up=list_)
+
+    def delete_records(self, selected_item:int)->None:
+        """Удаление записей"""
+        data = load_data()
+        del data[selected_item]
+        save_data(data)
+        self.records_window.destroy()
+        self.view_records(up=data)
 
 
 def main():
